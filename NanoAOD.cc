1c4cd843adcfccb6b4c7956c1bfbd585cc4e9f17
Philip Chang <sgnoohc@gmail.com>
2017-10-20 06:16:18 -0700

    Used TTree::MakeClass to generate and added few lines to declare global naod variable

--- NanoAOD.cc
+++ NanoAOD.cc
@@ -1,99 +1,44 @@
+#define NanoAOD_cxx
 #include "NanoAOD.h"
-NanoAOD nano;
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+NanoAOD naod;
 
-void NanoAOD::Init(TTree *tree) {
+void NanoAOD::Loop()
+{
+//   In a ROOT session, you can do:
+//      root> .L NanoAOD.C
+//      root> NanoAOD t
+//      root> t.GetEntry(12); // Fill t data members with entry number 12
+//      root> t.Show();       // Show values of entry 12
+//      root> t.Show(16);     // Read and show values of entry 16
+//      root> t.Loop();       // Loop on all entries
+//
 
-  tree->SetMakeClass(1);
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
 
-  run_branch = tree->GetBranch("run");
-  if (run_branch) run_branch->SetAddress(&run_);
-  luminosityBlock_branch = tree->GetBranch("luminosityBlock");
-  if (luminosityBlock_branch) luminosityBlock_branch->SetAddress(&luminosityBlock_);
-  event_branch = tree->GetBranch("event");
-  if (event_branch) event_branch->SetAddress(&event_);
+   Long64_t nentries = fChain->GetEntriesFast();
 
-  tree->SetMakeClass(0);
-}
-
-void NanoAOD::GetEntry(unsigned int idx) {
-  // this only marks branches as not loaded, saving a lot of time
-  index = idx;
-  run_isLoaded = false;
-  luminosityBlock_isLoaded = false;
-  event_isLoaded = false;
-}
-
-void NanoAOD::LoadAllBranches() {
-  // load all branches
-  if (run_branch != 0) run();
-  if (luminosityBlock_branch != 0) luminosityBlock();
-  if (event_branch != 0) event();
-}
-
-const unsigned int &NanoAOD::run() {
-  if (not run_isLoaded) {
-    if (run_branch != 0) {
-      run_branch->GetEntry(index);
-    } else {
-      printf("branch run_branch does not exist!\n");
-      exit(1);
-    }
-    run_isLoaded = true;
-  }
-  return run_;
-}
-
-const unsigned int &NanoAOD::luminosityBlock() {
-  if (not luminosityBlock_isLoaded) {
-    if (luminosityBlock_branch != 0) {
-      luminosityBlock_branch->GetEntry(index);
-    } else {
-      printf("branch luminosityBlock_branch does not exist!\n");
-      exit(1);
-    }
-    luminosityBlock_isLoaded = true;
-  }
-  return luminosityBlock_;
-}
-
-const unsigned long long &NanoAOD::event() {
-  if (not event_isLoaded) {
-    if (event_branch != 0) {
-      event_branch->GetEntry(index);
-    } else {
-      printf("branch event_branch does not exist!\n");
-      exit(1);
-    }
-    event_isLoaded = true;
-  }
-  return event_;
-}
-
-
-void NanoAOD::progress( int nEventsTotal, int nEventsChain ){
-  int period = 1000;
-  if (nEventsTotal%1000 == 0) {
-    // xterm magic from L. Vacavant and A. Cerri
-    if (isatty(1)) {
-      if ((nEventsChain - nEventsTotal) > period) {
-        float frac = (float)nEventsTotal/(nEventsChain*0.01);
-        printf("\015\033[32m ---> \033[1m\033[31m%4.1f%%"
-             "\033[0m\033[32m <---\033[0m\015", frac);
-        fflush(stdout);
-      }
-      else {
-        printf("\015\033[32m ---> \033[1m\033[31m%4.1f%%"
-               "\033[0m\033[32m <---\033[0m\015", 100.);
-        cout << endl;
-      }
-    }
-  }
-}
-
-namespace nanoAOD {
-
-const unsigned int &run() { return nano.run(); }
-const unsigned int &luminosityBlock() { return nano.luminosityBlock(); }
-const unsigned long long &event() { return nano.event(); }
-
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+   }
 }
